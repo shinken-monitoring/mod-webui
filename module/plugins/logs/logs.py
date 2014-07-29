624f748f97dd36720595c46ffda2768526fcfff8
Frédéric MOHIER <frederic.mohier@ipmfrance.com>
2014-07-29 19:40:50 +0200

    New plugin logs

---
+++ module/plugins/logs/logs.py
@@ -0,0 +1,268 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2009-2014:
+#    Gabes Jean, naparuba@gmail.com
+#    Gerhard Lausser, Gerhard.Lausser@consol.de
+#    Gregory Starck, g.starck@gmail.com
+#    Hartmut Goebel, h.goebel@goebel-consult.de
+#
+# This file is part of Shinken.
+#
+# Shinken is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Shinken is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
+
+import time
+
+from shinken.log import logger
+
+# Mongodb lib
+try:
+    import pymongo
+    from pymongo.connection import Connection
+    import gridfs
+except ImportError:
+    Connection = None
+
+
+### Will be populated by the UI with it's own value
+app = None
+
+# Get plugin's parameters from configuration file
+params = {}
+params['mongo_host'] = "localhost"
+params['mongo_port'] = 27017
+params['db_name'] = "Logs"
+params['logs_limit'] = 500
+params['logs_type'] = []
+params['logs_hosts'] = []
+params['logs_services'] = []
+
+def load_cfg():
+    global params
+    
+    import os,sys
+    from config_parser import config_parser
+    plugin_name = os.path.splitext(os.path.basename(__file__))[0]
+    try:
+        currentdir = os.path.dirname(os.path.realpath(__file__))
+        configuration_file = "%s/%s" % (currentdir, 'plugin.cfg')
+        logger.debug("Plugin configuration file: %s", configuration_file)
+        scp = config_parser('#', '=')
+        params = scp.parse_config(configuration_file)
+
+        # mongo_host = params['mongo_host']
+        params['mongo_port'] = int(params['mongo_port'])
+        params['logs_limit'] = int(params['logs_limit'])
+        params['logs_type'] = [item.strip() for item in params['logs_type'].split(',')]
+        params['logs_hosts'] = [item.strip() for item in params['logs_hosts'].split(',')]
+        params['logs_services'] = [item.strip() for item in params['logs_services'].split(',')]
+        
+        logger.debug("WebUI plugin '%s', configuration loaded." % (plugin_name))
+        logger.debug("Plugin %s configuration, database: %s (%s)", plugin_name, params['mongo_host'], params['mongo_port'])
+        logger.debug("Plugin %s configuration, fetching: %d %s", plugin_name, params['logs_limit'], params['logs_type'])
+        logger.debug("Plugin %s configuration, hosts: %s", plugin_name, params['logs_hosts'])
+        logger.debug("Plugin %s configuration, services: %s", plugin_name, params['logs_services'])
+        return True
+    except Exception, exp:
+        logger.warning("WebUI plugin '%s', configuration file (%s) not available: %s", plugin_name, configuration_file, str(exp))
+        return False
+
+
+def reload_cfg():
+    load_cfg()
+    app.bottle.redirect("/logs")
+
+
+def checkauth():
+    user = app.get_user_auth()
+
+    if not user:
+        app.bottle.redirect("/user/login")
+    else:
+        return user
+
+
+def getdb(dbname):
+    con = None
+    db = None
+
+    try:
+        con = Connection(params['mongo_host'],int(params['mongo_port']))
+    except:
+        return (  
+            "Error : Unable to create mongo DB connection %s:%s" % (params['mongo_host'],params['mongo_port']),
+            None
+        )
+
+    try:
+        db = con[dbname]
+    except:
+        return (  
+            "Error : Unable to connect to mongo database %s" % dbname,
+            None
+        )
+
+    return (  
+        "Connected to mongo database '%s'" % dbname,
+        db
+    )
+
+
+def show_logs():
+    user = checkauth()    
+
+    message,db = getdb(params['db_name'])
+    if not db:
+        return {
+            'app': app,
+            'user': user, 
+            'message': message,
+            'params': params,
+            'records': []
+        }
+
+    records=[]
+
+    try:
+        logger.info("[Logs] Fetching records from database: %s / %s / %s (max %d)" % (params['logs_type'], params['logs_hosts'], params['logs_services'], params['logs_limit']))
+
+        logs_limit = params['logs_limit']
+        logs_type = params['logs_type']
+        logs_hosts = params['logs_hosts']
+        logs_services = params['logs_services']
+
+        query = []
+        if len(logs_type) > 0 and logs_type[0] != '':
+            query.append({ "type" : { "$in": logs_type }})
+        if len(logs_hosts) > 0 and logs_hosts[0] != '':
+            query.append({ "host_name" : { "$in": logs_hosts }})
+        if len(logs_services) > 0 and logs_services[0] != '':
+            query.append({ "service_description" : { "$in": logs_services }})
+            
+        records=[]
+        if len(query) > 0:
+            for log in db.logs.find({'$and': query}).sort("time",-1).limit(logs_limit):
+                records.append({
+                    "date" : int(log["time"]),
+                    "host" : log['host_name'],
+                    "service" : log['service_description'],
+                    "message" : log['message']
+                })
+        else:
+            for log in db.logs.find().sort("time",-1).limit(logs_limit):
+                records.append({
+                    "date" : int(log["time"]),
+                    "host" : log['host_name'],
+                    "service" : log['service_description'],
+                    "message" : log['message']
+                })
+        message = "%d records fetched from database." % len(records)
+        logger.info("[Logs] %d records fetched from database.", len(records))
+    except Exception, exp:
+        logger.error("[Logs] Exception when querying database: %s", str(exp))
+
+    return {
+        'app': app,
+        'user': user, 
+        'message': message,
+        'params': params,
+        'records': records
+    }
+
+
+def form_hosts_list():
+    user = checkauth()    
+
+    return {'app': app, 'user': user, 'params': params}
+
+def set_hosts_list():
+    user = checkauth()    
+
+    # Form cancel
+    if app.request.forms.get('cancel'): 
+        app.bottle.redirect("/logs")
+
+    params['logs_hosts'] = []
+    
+    hostsList = app.request.forms.getall('hostsList[]')
+    logger.debug("Selected hosts : ")
+    for host in hostsList:
+        logger.debug("- host : %s" % (host))
+        params['logs_hosts'].append(host)
+
+    app.bottle.redirect("/logs")
+    return
+
+def form_services_list():
+    user = checkauth()    
+
+    return {'app': app, 'user': user, 'params': params}
+
+def set_services_list():
+    user = checkauth()    
+
+    # Form cancel
+    if app.request.forms.get('cancel'): 
+        app.bottle.redirect("/logs")
+
+    params['logs_services'] = []
+    
+    servicesList = app.request.forms.getall('servicesList[]')
+    logger.debug("Selected services : ")
+    for service in servicesList:
+        logger.debug("- service : %s" % (service))
+        params['logs_services'].append(service)
+
+    app.bottle.redirect("/logs")
+    return
+
+def form_logs_type_list():
+    user = checkauth()    
+
+    return {'app': app, 'user': user, 'params': params}
+
+def set_logs_type_list():
+    user = checkauth()    
+
+    # Form cancel
+    if app.request.forms.get('cancel'): 
+        app.bottle.redirect("/logs")
+
+    params['logs_type'] = []
+    
+    logs_typeList = app.request.forms.getall('logs_typeList[]')
+    logger.debug("Selected logs types : ")
+    for log_type in logs_typeList:
+        logger.debug("- log type : %s" % (log_type))
+        params['logs_type'].append(log_type)
+
+    app.bottle.redirect("/logs")
+    return
+
+
+load_cfg()
+
+pages = {   
+    reload_cfg: {'routes': ['/logs/reload'], 'view': '', 'static': True},
+
+    show_logs: {'routes': ['/logs'], 'view': 'logs', 'static': True},
+    
+    form_hosts_list: {'routes': ['/logs/hosts_list'], 'view': 'form_hosts_list'},
+    set_hosts_list: {'routes': ['/logs/set_hosts_list'], 'view': 'logs', 'method': 'POST'},
+    form_services_list: {'routes': ['/logs/services_list'], 'view': 'form_services_list'},
+    set_services_list: {'routes': ['/logs/set_services_list'], 'view': 'logs', 'method': 'POST'},
+    form_logs_type_list: {'routes': ['/logs/logs_type_list'], 'view': 'form_logs_type_list'},
+    set_logs_type_list: {'routes': ['/logs/set_logs_type_list'], 'view': 'logs', 'method': 'POST'},
+}
---
+++ module/plugins/logs/views/logs.tpl
@@ -0,0 +1,211 @@
+%rebase layout globals(), css=['logs/css/logs.css','logs/css/sliding_navigation.css','logs/css/bootstrap-multiselect.css'], js=['logs/js/bootstrap-multiselect.js'], title='System logs'
+
+%from shinken.bin import VERSION
+%helper = app.helper
+%import time
+%import datetime
+
+%today = datetime.datetime.now()
+%today_beginning = datetime.datetime(today.year, today.month, today.day,0,0,0,0)
+%today_beginning_time = int(time.mktime(today_beginning.timetuple()))
+%today_end = datetime.datetime(today.year, today.month, today.day,23,59,59,999)
+%today_end_time = int(time.mktime(today_end.timetuple()))
+
+%yesterday = datetime.datetime.now() - datetime.timedelta(days = 1)
+%yesterday_beginning = datetime.datetime(yesterday.year, yesterday.month, yesterday.day,0,0,0,0)
+%yesterday_beginning_time = int(time.mktime(yesterday_beginning.timetuple()))
+%yesterday_end = datetime.datetime(yesterday.year, yesterday.month, yesterday.day,23,59,59,999)
+%yesterday_end_time = int(time.mktime(yesterday_end.timetuple()))
+
+%thisweek = datetime.datetime.now() - datetime.timedelta(days = 7)
+%thisweek_beginning = datetime.datetime(thisweek.year, thisweek.month, thisweek.day,0,0,0,0)
+%thisweek_beginning_time = int(time.mktime(thisweek_beginning.timetuple()))
+%thisweek_end = thisweek_beginning + datetime.timedelta(days = 7)
+%thisweek_end_time = int(time.mktime(thisweek_end.timetuple()))
+
+%lastweek = datetime.datetime.now() - datetime.timedelta(days = 14)
+%lastweek_beginning = datetime.datetime(lastweek.year, lastweek.month, lastweek.day,0,0,0,0)
+%lastweek_beginning_time = int(time.mktime(lastweek_beginning.timetuple()))
+%lastweek_end = lastweek_beginning + datetime.timedelta(days = 7)
+%lastweek_end_time = int(time.mktime(lastweek_end.timetuple()))
+
+%thismonth = datetime.datetime.now() - datetime.timedelta(days = 31)
+%thismonth_beginning = datetime.datetime(thismonth.year, thismonth.month, thismonth.day,0,0,0,0)
+%thismonth_beginning_time = int(time.mktime(thismonth_beginning.timetuple()))
+%thismonth_end = thismonth_beginning + datetime.timedelta(days = 31)
+%thismonth_end_time = int(time.mktime(thismonth_end.timetuple()))
+
+<ul class="sliding-navigation drop-shadow" id="parameters">
+  <li class="sliding-element"><h3>Parameters</h3></li>
+  <li class="sliding-element">
+    <a><i class="fa fa-gear"></i> Logs limit: {{params['logs_limit']}}</a>
+  </li>
+  %if len(params['logs_hosts']) > 0:
+  <li class="sliding-element">
+    <a href="/logs/hosts_list" data-toggle="modal" data-target="#modal"><i class="fa fa-gear"></i> Hosts filter: 
+    <ul>
+    %for log_host in params['logs_hosts']:
+      <li class="sliding-element">{{log_host}}</li>
+    %end
+    </ul>
+    </a>
+  </li>
+  %end
+  %if len(params['logs_services']) > 0:
+  <li class="sliding-element">
+    <a href="/logs/services_list" data-toggle="modal" data-target="#modal"><i class="fa fa-gear"></i> Services filter: 
+    <ul>
+    %for log_service in params['logs_services']:
+      <li class="sliding-element">{{log_service}}</li>
+    %end
+    </ul>
+    </a>
+  </li>
+  %end
+  %if len(params['logs_type']) > 0:
+  <li class="sliding-element">
+    <a href="/logs/logs_type_list" data-toggle="modal" data-target="#modal"><i class="fa fa-gear"></i> Logs type filter: 
+    <ul>
+    %for log_type in params['logs_type']:
+      <li class="sliding-element">{{log_type}}</li>
+    %end
+    </ul>
+    </a>
+  </li>
+  %end
+</ul>
+<script type="text/javascript">
+  $("#parameters").draggable({
+    handle: ".modal-header"
+  });
+  
+  $(document).ready(function() {
+    $('.multiselect').multiselect();
+  });
+
+  function getHostsList(url){
+    // this code will send a data object via a GET request and alert the retrieved data.
+    $.jsonp({
+      "url": url+'?callback=?',
+      "success": function (response){
+        if (response.status == 200) {
+          alert(response.text);
+        }else{
+          alert(response.text);
+        }
+      },
+      "error": function (response) {
+        alert('Error !');
+      }
+    });
+  }
+</script>
+
+<ul class="nav nav-tabs" id="myTab">
+  <li class="active"><a href="#today" data-toggle="tab">Today</a></li>
+  <li><a href="#yesterday" data-toggle="tab">Yesterday</a></li>
+  <li><a href="#thisweek" data-toggle="tab">This week</a></li>
+  <li><a href="#lastweek" data-toggle="tab">Last week</a></li>
+  <li><a href="#custom" data-toggle="tab">This month</a></li>
+</ul>
+
+<div class="tab-content">
+  <div class="tab-pane active" id="today">
+    <table class="table table-striped">
+      <tbody>
+        <tr>
+          <td colspan="4"><em>{{message}}</em></td>
+        </tr>
+        <tr>
+          <td colspan="4"><strong>From {{time.asctime(time.localtime(today_beginning_time))}} to {{time.asctime(time.localtime(today_end_time))}}</strong></td>
+        </tr>
+%for log in records:
+%if log['date'] >= today_beginning_time and log['date'] <= today_end_time:
+        <tr>
+          <td class="col-sm-1">{{time.asctime(time.localtime(log['date']))}}</td>
+          <td class="col-sm-1">{{log['host']}}</td>
+          <td class="col-sm-1">{{log['service']}}</td>
+          <td class="col-sm-8">{{log['message']}}</td>
+        </tr>
+%end
+%end
+      </tbody>
+    </table>
+  </div>
+  <div class="tab-pane" id="yesterday">
+    <table class="table table-striped">
+      <tbody>
+        <tr>
+          <td colspan="4"><strong>From {{time.asctime(time.localtime(yesterday_beginning_time))}} to {{time.asctime(time.localtime(yesterday_end_time))}}</strong></td>
+        </tr>
+%for log in records:
+%if log['date'] >= yesterday_beginning_time and log['date'] <= yesterday_end_time:
+        <tr>
+          <td>{{time.asctime(time.localtime(log['date']))}}</td>
+          <td>{{log['host']}}</td>
+          <td>{{log['service']}}</td>
+          <td>{{log['message']}}</td>
+        </tr>
+%end
+%end
+      </tbody>
+    </table>
+  </div>
+  <div class="tab-pane" id="thisweek">
+    <table class="table table-striped">
+      <tbody>
+        <tr>
+          <td colspan="4"><strong>From {{time.asctime(time.localtime(thisweek_beginning_time))}} to {{time.asctime(time.localtime(thisweek_end_time))}}</strong></td>
+        </tr>
+%for log in records:
+%if log['date'] >= thisweek_beginning_time and log['date'] <= thisweek_end_time:
+        <tr>
+          <td>{{time.asctime(time.localtime(log['date']))}}</td>
+          <td>{{log['host']}}</td>
+          <td>{{log['service']}}</td>
+          <td>{{log['message']}}</td>
+        </tr>
+%end
+%end
+      </tbody>
+    </table>
+  </div>
+  <div class="tab-pane" id="lastweek">
+    <table class="table table-striped">
+      <tbody>
+        <tr>
+          <td colspan="4"><strong>From {{time.asctime(time.localtime(lastweek_beginning_time))}} to {{time.asctime(time.localtime(lastweek_end_time))}}</strong></td>
+        </tr>
+%for log in records:
+%if log['date'] >= lastweek_beginning_time and log['date'] <= lastweek_end_time:
+        <tr>
+          <td>{{time.asctime(time.localtime(log['date']))}}</td>
+          <td>{{log['host']}}</td>
+          <td>{{log['service']}}</td>
+          <td>{{log['message']}}</td>
+        </tr>
+%end
+%end
+      </tbody>
+    </table>
+  </div>
+  <div class="tab-pane" id="lastmonth">
+    <table class="table table-striped">
+      <tbody>
+        <tr>
+          <td colspan="4"><strong>From {{time.asctime(time.localtime(thismonth_beginning_time))}} to {{time.asctime(time.localtime(thismonth_end_time))}}</strong></td>
+        </tr>
+%for log in records:
+%if log['date'] >= thismonth_beginning_time and log['date'] <= thismonth_end_time:
+        <tr>
+          <td>{{time.asctime(time.localtime(log['date']))}}</td>
+          <td>{{log['host']}}</td>
+          <td>{{log['service']}}</td>
+          <td>{{log['message']}}</td>
+        </tr>
+%end
+%end
+      </tbody>
+    </table>
+  </div>
+</div>
