a319f24f6c56e44698446acd24c176a38b7a3fb1
Frédéric MOHIER <frederic.mohier@ipmfrance.com>
2014-08-01 19:35:33 +0200

    Groups view improvements
    Fix small CSS/JS bugs
    In overview, groups are sorted ascending on name

--- module/plugins/groups/htdocs/js/groups-overview.js
+++ module/plugins/groups/htdocs/js/groups-overview.js
@@ -7,9 +7,6 @@ $(document).ready(function(){
 		var thegroups = $("ul#groups");
 		var classNames = $(this).attr('class').split(' ');
 		
-		var gridthumb = "images/groups/grid-default-thumb.png";
-		var listthumb = "images/groups/list-default-thumb.png";
-		
 		if($(this).hasClass("active")) {
 			// if currently clicked button has the active class
 			// then we do nothing!
@@ -18,39 +15,21 @@ $(document).ready(function(){
 			// otherwise we are clicking on the inactive button
 			// and in the process of switching views!
 
-  			if(theid == "gridview") {
+      if (theid == "gridview") {
 				$(this).addClass("active");
 				$("#listview").removeClass("active");
-			
-				$("#listview").children("img").attr("src","images/list-view.png");
-			
-				var theimg = $(this).children("img");
-				theimg.attr("src","images/grid-view-active.png");
 			
 				// remove the list class and change to grid
 				thegroups.removeClass("list");
 				thegroups.addClass("grid");
-			
-				// update all thumbnails to larger size
-				$("img.thumb").attr("src",gridthumb);
-			}
-			
-			else if(theid == "listview") {
+			} else if (theid == "listview") {
 				$(this).addClass("active");
 				$("#gridview").removeClass("active");
-					
-				$("#gridview").children("img").attr("src","images/grid-view.png");
-					
-				var theimg = $(this).children("img");
-				theimg.attr("src","images/list-view-active.png");
 					
 				// remove the grid view and change to list
 				thegroups.removeClass("grid")
 				thegroups.addClass("list");
-				// update all thumbnails to smaller size
-				$("img.thumb").attr("src",listthumb);
 			} 
 		}
-
 	});
 });
