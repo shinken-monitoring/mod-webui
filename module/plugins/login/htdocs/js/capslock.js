0bb1cbfc57fd73fb696c0de93dbe3e0d800a76f6
Frédéric MOHIER <frederic.mohier@ipmfrance.com>
2015-04-08 14:03:41 +0200

    Update login form and fix JS error

--- module/plugins/login/htdocs/js/capslock.js
+++ module/plugins/login/htdocs/js/capslock.js
@@ -21,11 +21,12 @@
  along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-function capsCheck(e,obj){ 
-    kc = e.keyCode?e.keyCode:e.which;  
-    sk = e.shiftKey?e.shiftKey:((kc == 16)?true:false);  
-    if(((kc >= 65 && kc <= 90) && !sk)||((kc >= 97 && kc <= 122) && sk)){
-        document.getElementById('#'+obj.id).style.visibility = 'visible';
-    } 
-    else document.getElementById('#'+obj.id).style.visibility = 'hidden';
-}​
+function capsCheck(e,obj) {
+	kc = e.keyCode?e.keyCode:e.which;
+	sk = e.shiftKey?e.shiftKey:((kc == 16)?true:false); 
+	if ( ((kc >= 65 && kc <= 90) && !sk) || ((kc >= 97 && kc <= 122) && sk) ) {
+		document.getElementById('#'+obj.id).style.visibility = 'visible';
+	} else {
+		document.getElementById('#'+obj.id).style.visibility = 'hidden';
+	}
+}
