91fa5b0ab21afdc26a0492acadf71147de85fe94
Frédéric MOHIER <frederic.mohier@ipmfrance.com>
2015-05-03 11:53:38 +0200

    Fix bookmarks refresh JS error

--- module/plugins/problems/htdocs/js/bookmarks.js
+++ module/plugins/problems/htdocs/js/bookmarks.js
@@ -1,118 +1,106 @@
 function add_new_bookmark(page){
-    // console.log('We will try to save the user bookmark');
-    var f = document.forms['bookmark_save'];
-    var name = f.bookmark_name.value;
-    if (name=='') return;
+   // console.log('We will try to save the user bookmark');
+   var f = document.forms['bookmark_save'];
+   var name = f.bookmark_name.value;
+   if (name=='') return;
 
-    // console.log('Saving a bookmark with: '+name);
+   var uri = get_current_search(page);
 
-    var uri = get_current_search(page);
-    // console.log('With the URI: '+uri);
-
-    var b = {'name' : name, 'uri' : uri};
-    // Do not save the bm if there is already one with this name
-    var names = new Array();
-    $.each(bookmarks, function(idx, bm){
-        names.push(bm.name);
-    });
-    if (names.indexOf(name)==-1) {
-        // Ok we can save bookmarks in our preferences
-        bookmarks.push(b);
-        save_bookmarks();
-    } else { 
+   var b = {'name' : name, 'uri' : uri};
+   
+   // Do not save the bm if there is already one with this name
+   var names = new Array();
+   $.each(bookmarks, function(idx, bm){
+      names.push(bm.name);
+   });
+   if (names.indexOf(name)==-1) {
+      // Ok we can save bookmarks in our preferences
+      bookmarks.push(b);
+      save_bookmarks();
+   } else { 
       alert('This bookmark already exists !');
-    }
+   }
 }
 
 function save_bookmarks(){
-    // console.log('Need to save bookmarks list: '+JSON.stringify(bookmarks));
-    $.post("/user/save_pref", { 'key' : 'bookmarks', 'value' : JSON.stringify(bookmarks)});
+   // console.log('Need to save bookmarks list: '+JSON.stringify(bookmarks));
+   $.post("/user/save_pref", { 'key' : 'bookmarks', 'value' : JSON.stringify(bookmarks)});
 
-    // And refresh it
-    refresh_bookmarks();
+   // And refresh it
+   refresh_bookmarks();
 }
 
 function save_bookmarksro(){
-    // console.log('Need to save common bookmarks list: '+JSON.stringify(bookmarks));
-    $.post("/user/save_common_pref", { 'key' : 'bookmarks', 'value' : JSON.stringify(bookmarksro)});
+   $.post("/user/save_common_pref", { 'key' : 'bookmarks', 'value' : JSON.stringify(bookmarksro)});
 
-    // And refresh it
-    refresh_bookmarksro();
+   // And refresh it
+   refresh_bookmarksro();
 }
 
 function push_to_common_bookmarks(name,uri) {
-    // console.log('Pushing a bookmark to common ones');
-    var b = {'name' : name, 'uri' : uri};
-    // Do not save the bm if there is already one with this name
-    var names = new Array();
-    $.each(bookmarksro, function(idx, bm){
-        names.push(bm.name);
-    });
-    if (names.indexOf(name)==-1) {
-        // Ok we can save bookmarks in our preferences
-        bookmarksro.push(b);
-        save_bookmarksro();
-    } else { 
-        alert('This Common bookmark name already exists !');
-    }
+   var b = {'name' : name, 'uri' : uri};
+   // Do not save the bm if there is already one with this name
+   var names = new Array();
+   $.each(bookmarksro, function(idx, bm){
+      names.push(bm.name);
+   });
+   if (names.indexOf(name)==-1) {
+      // Ok we can save bookmarks in our preferences
+      bookmarksro.push(b);
+      save_bookmarksro();
+   } else { 
+      alert('This Common bookmark name already exists !');
+   }
 }
 
 
 function declare_bookmark(name, uri){
-    // console.log('declaring bookmark '+name+' at uri '+uri);
-    var b = {'name' : name, 'uri' : uri};
-    bookmarks.push(b);
+   var b = {'name' : name, 'uri' : uri};
+   bookmarks.push(b);
 }
 
 function declare_bookmarksro(name, uri){
-    // console.log('declaring Common bookmark '+name+' at uri '+uri);
-    var b = {'name' : name, 'uri' : uri};
-    bookmarksro.push(b);
+   var b = {'name' : name, 'uri' : uri};
+   bookmarksro.push(b);
 }
 
 function refresh_bookmarks(){
-    if (bookmarks.length == 0){
-        $('#bookmarks').html('<ul class="list-group"><li class="list-group-item list-group-item-danger">No user bookmarks</li></ul>');
-        return;
-    }
+   if (bookmarks.length == 0){
+      $('#bookmarks').html('<ul class="list-group"><li class="list-group-item list-group-item-danger">No user bookmarks</li></ul>');
+      return;
+   }
 
-    s = 'Your bookmarks: <ul class="list-group">';
-    $.each(bookmarks, function(idx, b){
-        l = '<span><a href="'+b.uri+'"><i class="fa fa-tag"></i> '+b.name+'</a></span>';
-        fun = "delete_bookmark('"+b.name+"');";
-        c = '<span><a href="javascript:'+fun+'" class="close">&times;</a></span>';
-        if (advfct == 1) {
-                fun2 = "push_to_common_bookmarks('"+b.name+"','"+b.uri+"');";
-                c2 = '<span><a href="javascript:'+fun2+'" class="close">&plus;</a></span>';
-        }
-        else { c2 =""; }
+   s = 'Your bookmarks: <ul class="list-group">';
+   $.each(bookmarks, function(idx, b){
+      l = '<span><a href="'+b.uri+'"><i class="fa fa-tag"></i> '+b.name+'</a></span>';
+      fun = "delete_bookmark('"+b.name+"');";
+      c = '<span><a href="javascript:'+fun+'" class="close">&times;</a></span>';
+      fun2 = "push_to_common_bookmarks('"+b.name+"','"+b.uri+"');";
+      c2 = '<span><a href="javascript:'+fun2+'" class="close">&plus;</a></span>';
 
-        s+= '<li class="list-group-item">'+l+c+c2+'</li>';
-    });
-    s += '</ul>';
-    $('#bookmarks').html(s);
+      s+= '<li class="list-group-item">'+l+c+c2+'</li>';
+   });
+   s += '</ul>';
+   
+   $('#bookmarks').html(s);
 }
 
 function refresh_bookmarksro(){
+   if (bookmarksro.length == 0) {
+      $('#bookmarksro').html('<ul class="list-group"><li class="list-group-item list-group-item-danger">No common bookmarks</li></ul>');
+      return;
+   }
 
-    if(bookmarksro.length == 0){
-        $('#bookmarksro').html('<ul class="list-group"><li class="list-group-item list-group-item-danger">No common bookmarks</li></ul>');
-        return;
-    }
-
-    sro = 'Common bookmarks: <ul class="list-group">';
-    $.each(bookmarksro, function(idx, b){
-        l = '<span><a href="'+b.uri+'"><i class="fa fa-tag"></i> '+b.name+'</a></span>';
-        if (advfct == 1) {
-                fun = "delete_bookmarkro('"+b.name+"');";
-                c = '<span><a href="javascript:'+fun+'" class="close">&times;</a></span>';
-        }
-        else { c =""; }
-        sro+= '<li class="list-group-item">'+l+c+'</li>';
-
-    });
-    sro += '</ul>';
-    $('#bookmarksro').html(sro);
+   sro = 'Common bookmarks: <ul class="list-group">';
+   $.each(bookmarksro, function(idx, b) {
+      l = '<span><a href="'+b.uri+'"><i class="fa fa-tag"></i> '+b.name+'</a></span>';
+      fun = "delete_bookmarkro('"+b.name+"');";
+      c = '<span><a href="javascript:'+fun+'" class="close">&times;</a></span>';
+      sro+= '<li class="list-group-item">'+l+c+'</li>';
+   });
+   sro += '</ul>';
+   
+   $('#bookmarksro').html(sro);
 }
 
 
@@ -120,23 +108,30 @@ function refresh_bookmarksro(){
 
 // We want to delete one specific bookmark by its name
 function delete_bookmark(name){
-    new_bookmarks = [];
-    $.each(bookmarks, function(idx, b){
-        if(b.name != name){
-            new_bookmarks.push(b);
-        }
-    });
-    bookmarks = new_bookmarks;
-    save_bookmarks();
+   new_bookmarks = [];
+   $.each(bookmarks, function(idx, b){
+      if (b.name != name) {
+         new_bookmarks.push(b);
+      }
+   });
+   bookmarks = new_bookmarks;
+   save_bookmarks();
 }
 
 function delete_bookmarkro(name){
-    new_bookmarksro = [];
-    $.each(bookmarksro, function(idx, b){
-        if(b.name != name){
-            new_bookmarksro.push(b);
-        }
-    });
-    bookmarksro = new_bookmarksro;
-    save_bookmarksro();
+   new_bookmarksro = [];
+   $.each(bookmarksro, function(idx, b){
+      if (b.name != name) {
+         new_bookmarksro.push(b);
+      }
+   });
+   bookmarksro = new_bookmarksro;
+   save_bookmarksro();
 }
+
+// On page loaded ... 
+$(document).ready(function(){
+   // Refresh bookmarks
+   refresh_bookmarks(); 
+   refresh_bookmarksro();
+});
