faa00cb3795d8c18216125b592ff57865c35fa3c
Guillaume Subiron <maethor@subiron.org>
2015-06-15 18:20:22 +0200

    PEP8 and cleanup
    This commit removes tags and groups plugin configs.

--- module/plugins/groups/groups.py
+++ module/plugins/groups/groups.py
@@ -24,50 +24,26 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
 
-### Will be populated by the UI with it's own value
+# Will be populated by the UI with it's own value
 app = None
 
-# Get plugin's parameters from configuration file
-params = {}
-params['elts_per_page'] = 10
-
-def load_cfg():
-    global params
-    
-    import os
-    from shinken.log import logger
-    from webui.config_parser import config_parser
-    try:
-        currentdir = os.path.dirname(os.path.realpath(__file__))
-        configuration_file = "%s/%s" % (currentdir, 'plugin.cfg')
-        logger.debug("Plugin configuration file: %s" % (configuration_file))
-        scp = config_parser('#', '=')
-        params = scp.parse_config(configuration_file)
-
-        params['elts_per_page'] = int(params['elts_per_page'])
-        
-        logger.info("[webui-groups] configuration loaded.")
-        logger.debug("[webui-groups] configuration, elts_per_page: %d", params['elts_per_page'])
-        return True
-    except Exception, exp:
-        logger.warning("[webui-groups] configuration file (%s) not available: %s", configuration_file, str(exp))
-        return False
 
 def reload_cfg():
-    load_cfg()
     app.bottle.redirect("/config")
+
 
 def show_hostgroup(name):
     app.bottle.redirect("/all?search=type:host hg:" + name)
 
+
 def show_hostgroups():
-    user = app.check_user_authentication()    
+    user = app.check_user_authentication()
 
     # Set hostgroups level ...
     app.set_hostgroups_level(user)
-    
+
     return {
-        'app': app, 'user': user, 'params': params, 
+        'app': app, 'user': user,
         'hostgroups': sorted(app.get_hostgroups(), key=lambda hostgroup: hostgroup.hostgroup_name)
         }
 
@@ -75,26 +51,22 @@ def show_hostgroups():
 def show_servicegroup(name):
     app.bottle.redirect("/all?search=type:service hg:" + name)
 
+
 def show_servicegroups():
-    user = app.check_user_authentication()    
+    user = app.check_user_authentication()
 
     # Set servicegroups level ...
     app.set_servicegroups_level(user)
 
     return {
-        'app': app, 'user': user, 'params': params, 
+        'app': app, 'user': user,
         'servicegroups': sorted(app.get_servicegroups(), key=lambda servicegroup: servicegroup.servicegroup_name)
         }
 
-
-# Load plugin configuration parameters
-load_cfg()
-
 pages = {
-        reload_cfg: {'routes': ['/reload/groups']},
-        
-        show_hostgroup: {'routes': ['/hosts-group/:name'], 'view': 'hosts-group', 'static': True},
-        show_hostgroups: {'routes': ['/hosts-groups'], 'view': 'hosts-groups-overview', 'static': True},
-        show_servicegroup: {'routes': ['/services-group/:name'], 'view': 'services-group', 'static': True},
-        show_servicegroups: {'routes': ['/services-groups'], 'view': 'services-groups-overview', 'static': True},
-        }
+    reload_cfg: {'routes': ['/reload/groups']},
+    show_hostgroup: {'routes': ['/hosts-group/:name'], 'view': 'hosts-group', 'static': True},
+    show_hostgroups: {'routes': ['/hosts-groups'], 'view': 'hosts-groups-overview', 'static': True},
+    show_servicegroup: {'routes': ['/services-group/:name'], 'view': 'services-group', 'static': True},
+    show_servicegroups: {'routes': ['/services-groups'], 'view': 'services-groups-overview', 'static': True},
+}
--- module/plugins/tags/tags.py
+++ module/plugins/tags/tags.py
@@ -24,79 +24,52 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
 
-### Will be populated by the UI with it's own value
+# Will be populated by the UI with it's own value
 app = None
 
 from shinken.misc.filter import only_related_to
 
-# Get plugin's parameters from configuration file
-params = {}
-params['elts_per_page'] = 10
-
-def load_cfg():
-    global params
-
-    import os
-    from shinken.log import logger
-    from webui.config_parser import config_parser
-    plugin_name = os.path.splitext(os.path.basename(__file__))[0]
-    try:
-        currentdir = os.path.dirname(os.path.realpath(__file__))
-        configuration_file = "%s/%s" % (currentdir, 'plugin.cfg')
-        logger.debug("Plugin configuration file: %s" % (configuration_file))
-        scp = config_parser('#', '=')
-        params = scp.parse_config(configuration_file)
-
-        params['elts_per_page'] = int(params['elts_per_page'])
-
-        logger.debug("WebUI plugin '%s', configuration loaded." % (plugin_name))
-        logger.debug("Plugin configuration, elts_per_page: %d" % (params['elts_per_page']))
-
-        return True
-    except Exception, exp:
-        logger.warning("WebUI plugin '%s', configuration file (%s) not available: %s" % (plugin_name, configuration_file, str(exp)))
-        return False
 
 def reload_cfg():
-    load_cfg()
     app.bottle.redirect("/config")
+
 
 def show_tag(name):
     app.bottle.redirect("/all?search=type:host htag:" + name)
 
+
 def show_stag(name):
     app.bottle.redirect("/all?search=type:service stag:" + name)
+
 
 def show_tags():
     user = app.check_user_authentication()
 
     fake_htags = []
     for tag in app.get_host_tags_sorted():
-        hosts = only_related_to(app.get_hosts_tagged_with(tag[0]),user)
+        hosts = only_related_to(app.get_hosts_tagged_with(tag[0]), user)
         if len(hosts) > 0:
             fake_htags.append({'name': tag[0], 'hosts': hosts})
 
-    return {'app': app, 'user': user, 'params': params, 'htags': fake_htags}
+    return {'app': app, 'user': user, 'htags': fake_htags}
+
 
 def show_stags():
     user = app.check_user_authentication()
 
     fake_stags = []
     for tag in app.get_service_tags_sorted():
-        services = only_related_to(app.get_services_tagged_with(tag[0]),user)
+        services = only_related_to(app.get_services_tagged_with(tag[0]), user)
         if len(services) > 0:
             fake_stags.append({'name': tag[0], 'services': services})
 
-    return {'app': app, 'user': user, 'params': params, 'stags': fake_stags}
+    return {'app': app, 'user': user, 'stags': fake_stags}
 
-# Load plugin configuration parameters
-load_cfg()
 
 pages = {
-        reload_cfg: {'routes': ['/reload/tags']},
-
-        show_tag: {'routes': ['/hosts-tag/:name'], 'view': 'hosts-tag', 'static': True},
-        show_stag: {'routes': ['/services-tag/:name'], 'view': 'services-tag', 'static': True},
-        show_tags: {'routes': ['/hosts-tags'], 'view': 'hosts-tags-overview', 'static': True},
-        show_stags: {'routes': ['/services-tags'], 'view': 'services-tags-overview', 'static': True},
-        }
+    reload_cfg: {'routes': ['/reload/tags']},
+    show_tag: {'routes': ['/hosts-tag/:name'], 'view': 'hosts-tag', 'static': True},
+    show_stag: {'routes': ['/services-tag/:name'], 'view': 'services-tag', 'static': True},
+    show_tags: {'routes': ['/hosts-tags'], 'view': 'hosts-tags-overview', 'static': True},
+    show_stags: {'routes': ['/services-tags'], 'view': 'services-tags-overview', 'static': True},
+}
