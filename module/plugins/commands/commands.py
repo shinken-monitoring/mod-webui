7680b0c843561b137df651c6fbe9b2924c5b2183
Frédéric MOHIER <frederic.mohier@gmail.com>
2015-07-02 03:17:02 +0200

    Cleaned commands page

--- module/plugins/commands/commands.py
+++ module/plugins/commands/commands.py
@@ -26,57 +26,15 @@
 ### Will be populated by the UI with it's own value
 app = None
 
-import time
-
-from shinken.util import safe_print
-
-# Get plugin's parameters from configuration file
-params = {}
-params['display'] = ''
-
-def load_cfg():
-    global params
-    
-    import os,sys
-    from webui.config_parser import config_parser
-    from shinken.log import logger
-    plugin_name = os.path.splitext(os.path.basename(__file__))[0]
-    try:
-        currentdir = os.path.dirname(os.path.realpath(__file__))
-        configuration_file = "%s/%s" % (currentdir, 'plugin.cfg')
-        logger.debug("Plugin configuration file: %s" % (configuration_file))
-        scp = config_parser('#', '=')
-        params = scp.parse_config(configuration_file)
-
-        params['display'] = params['display']
-        
-        logger.info("[webui-commands] configuration loaded.")
-        logger.debug("[webui-commands] configuration, display: %s", params['display'])
-        return True
-    except Exception, exp:
-        logger.warning("[webui-commands] configuration file (%s) not available: %s", configuration_file, str(exp))
-        return False
-
-def reload_cfg():
-    load_cfg()
-    app.bottle.redirect("/config")
-
-
 # All commands
 def show_commands():
     user = app.check_user_authentication()
 
     return {
-        'app': app, 'user': user, 'params': params, 
+        'app': app, 'user': user, 
         'commands': sorted(app.get_commands(), key=lambda command: command.command_name)
         }
 
-
-# Load plugin configuration parameters
-load_cfg()
-
 pages = {
-        reload_cfg: {'routes': ['/reload/commands']},
-        
         show_commands: {'routes': ['/commands'], 'view': 'commands', 'static': True},
-        }
+}
