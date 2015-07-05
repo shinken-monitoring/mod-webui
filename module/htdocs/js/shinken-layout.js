57003577f0eda18d9284b1ff00ff641e475f0ed3
Frédéric MOHIER <frederic.mohier@gmail.com>
2015-07-05 20:44:41 +0200

    Toggle list in business rule tree

--- module/htdocs/js/shinken-layout.js
+++ module/htdocs/js/shinken-layout.js
@@ -125,6 +125,22 @@ $(document).ready(function(){
       // Hide sidebar menu ...
       $('.sidebar').hide();
    });
+   
+   // When toggle list is activated ...
+   $('a.toggle-list').on('click', function () {
+      var state = $(this).data('state');
+      var target = $(this).data('target');
+      
+      if (state=='expanded') {
+         $('#'+target).hide();
+         $(this).data('state', 'collapsed')
+         $(this).children('i').removeClass('fa-minus').addClass('fa-plus');
+      } else {
+         $('#'+target).show();
+         $(this).data('state', 'expanded')
+         $(this).children('i').removeClass('fa-plus').addClass('fa-minus');
+      }
+   });
 
    // Sidebar menu
    $('#sidebar-menu').metisMenu();
