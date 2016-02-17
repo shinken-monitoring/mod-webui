f73cb2426badb1508d04d6ce60f4aa4a6650329f
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-02-17 19:46:14 +0100

    Set version number to 2.3.0

--- module/module.py
+++ module/module.py
@@ -25,9 +25,9 @@
 # along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
 
 
-WEBUI_VERSION = "2.2.1"
-WEBUI_COPYRIGHT = "(c) 2009-2015 - License GNU AGPL as published by the FSF, minimum version 3 of the License."
-WEBUI_RELEASENOTES = """Bootstrap 3 User Interface - complete User Interface refactoring"""
+WEBUI_VERSION = "2.3.0"
+WEBUI_COPYRIGHT = "(c) 2009-2016 - License GNU AGPL as published by the FSF, minimum version 3 of the License."
+WEBUI_RELEASENOTES = """Heavy work on dashboard currently view ... use and abuse Eye icon in the header toolbar."""
 
 
 """
