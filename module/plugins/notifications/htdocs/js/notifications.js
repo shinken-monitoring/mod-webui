501acd3b7562e3b1c00bad451d120ab4b5ab2448
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-04-15 19:46:10 +0200

    Global notifications
    Thanks to @nchowning (former PR #470)

---
+++ module/plugins/notifications/htdocs/js/notifications.js
@@ -0,0 +1,39 @@
+/* Copyright (C) 2009-2015:
+   Gabes Jean, naparuba@gmail.com
+   Gerhard Lausser, Gerhard.Lausser@consol.de
+   Gregory Starck, g.starck@gmail.com
+   Hartmut Goebel, h.goebel@goebel-consult.de
+   Andreas Karfusehr, andreas@karfusehr.de
+   Frederic Mohier, frederic.mohier@gmail.com
+
+   This file is part of Shinken.
+
+   Shinken is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Shinken is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/*
+ * Function called when the page is loaded and on each page refresh ...
+ */
+function on_page_refresh() {
+   // Buttons as switches
+   $('input.switch').bootstrapSwitch();
+
+   // Toggles Notifications
+   $('input[action="toggle-notifications"]').on('switchChange.bootstrapSwitch', function (event, state) {
+      toggle_all_notifications(state==false ? true : false);
+   });
+}
+
+on_page_refresh();
