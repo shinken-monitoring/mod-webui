0018e17aa9079ba685f5dc89f514bce2d23b6181
Frédéric MOHIER <frederic.mohier@gmail.com>
2018-09-06 05:11:50 +0200

    #620 Use Shinken WebUI with the Alignak framework
    * Very first version tested locally - looks like everything is ok
    
    * Some Regenerator fixes and improvements
    - timeperiods
    - downtimes and comments for hosts/services
    
    * Fix #621 - fix external commands for host/service check results
    
    * Fix WebUI modules loading
    
    * Fix service display_name property

--- module/__init__.py
+++ module/__init__.py
@@ -21,3 +21,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
+
+# Alignak need this information
+# the properties and get_instance must be available from the top-level:
+from .module import properties, get_instance
